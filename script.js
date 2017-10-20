6d92321d99cd28e5a5ee36a86ecd6915e40fc428
Elvanunal <32845190+Elvanunal@users.noreply.github.com>
2017-10-20 15:01:50 +0200

    DERNIER COMMIT
    nettoyage des codes
    ajout d'un article

--- script.js
+++ script.js
@@ -1,54 +1,38 @@
 $(document).ready(function(){
 
-$("header").hide();
-$("header").fadeIn(2000);
+
+  //* ANIMATION HIT THE RADIO cacher -> transparence *//
+  $("header").hide();
+  $("header").fadeIn(6000);
 });
 
-$.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/musics?api_key=keyu2Ub4GvFkI7UKc",
-                    function(data) {
-              console.log(data);
-            $.each(data.records, function(key,val){
 
-$("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#toplistapi');
+  //* TOP LIST API *//
+  $.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/musics?api_key=keyu2Ub4GvFkI7UKc",
+    function(data) {
+    console.log(data);
+  $.each(data.records, function(key,val){
 
+    $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#toplistapi');
            });
                     }
 );
 
 
 
+  //* api DE LA CATEGORIE PLAYLISTS *//
+  $.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/playlists?api_key=keyu2Ub4GvFkI7UKc",
+    function(data) {
+    console.log(data);
+  $.each(data.records, function(key,val){
 
-$.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/playlists?api_key=keyu2Ub4GvFkI7UKc",
-                    function(data) {
-              console.log(data);
-            $.each(data.records, function(key,val){
-
-$("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#playlists');
-
-           });
-                    }
-);
-
-
-
-
-$.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/albums?api_key=keyu2Ub4GvFkI7UKc",
-                    function(data) {
-              console.log(data);
-            $.each(data.records, function(key,val){
-
-$("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#albums');
+    $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#playlists');
 
            });
                     }
 );
 
-
-
-
-
-
-
+//* api DE LA PLAYLIST2*//
 
 $.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/playlists2?api_key=keyu2Ub4GvFkI7UKc",
                     function(data) {
@@ -56,6 +40,19 @@ $.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/playlists2?api_key=keyu
             $.each(data.records, function(key,val){
 
 $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#playlists2');
+
+           });
+                    }
+);
+
+  //* api DES ALBUMS*//
+
+  $.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/albums?api_key=keyu2Ub4GvFkI7UKc",
+      function(data) {
+      console.log(data);
+  $.each(data.records, function(key,val){
+
+    $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#albums');
 
            });
                     }
